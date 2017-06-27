b771f5c38e5a99df3ec1bcef219994684bbe7bc0
Javier Rotelli <javier.rotelli@gmail.com>
2017-06-27 18:28:14 -0300

    flujo de oauth inicial andando

--- src/Login/Container.jsx
+++ src/Login/Container.jsx
@@ -2,9 +2,10 @@ import { connect } from 'react-redux';
 import { doLogin, doLogout } from './Actions';
 import Login from './Login';
 
-const mapStateToProps = ({ auth }) => ({
+const mapStateToProps = ({ auth, utnianosAuth }) => ({
   isLoggedIn: auth.isLoggedIn,
-  isLoggingIn: auth.isLoggingIn
+  isLoggingIn: auth.isLoggingIn,
+  providers: utnianosAuth.providers
 });
 
 const mapDispatchToProps = {
